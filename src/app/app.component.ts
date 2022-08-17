8459545f1b171aed225e9b96d7c8ec8aee8e12e1
Gean <geanp578@gmail.com>
2022-08-17 18:27:08 -0300

    add Lista1 Finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularProject';
+  title: string = "angularProject";
+
+  textoSalvo: string = "";
+  cliqueBtEnviar(campoInput: string) {
+    this.textoSalvo = campoInput
+
+  }
 }
+
