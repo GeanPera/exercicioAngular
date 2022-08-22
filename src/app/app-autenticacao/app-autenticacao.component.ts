47e8b8e605c7226c15e0288f8ca92d70b7dcbe27
Gean <geanp578@gmail.com>
2022-08-21 23:23:40 -0300

    add spinner e ngClass no paragrafo msn

--- src/app/app-autenticacao/app-autenticacao.component.ts
+++ src/app/app-autenticacao/app-autenticacao.component.ts
@@ -1,42 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Usuario } from "../interfaces/usuario/usuario";
+
 @Component({
   selector: 'app-app-autenticacao',
   templateUrl: './app-autenticacao.component.html',
   styleUrls: ['./app-autenticacao.component.css']
 })
-export class AppAutenticacaoComponent implements OnInit {
+export class AppAutenticacaoComponent{
 
-  msn:string = "";
-  email: string = "";
-  password: string = "";
-  emailCadastrado: string = "XPTO-21";
-  passwordCadastrado: string = "Trocar@123";
-  color: string = "black";
-  contador:number = 0
-  
-
-  
-  constructor() { }
-
-  ngOnInit(): void {
+  usuario: Usuario = {
+    userId: "",
+    password: "",
+    tipo: ""
   }
-
+  classe = {
+    'color' : ""
+  }
+  msn?:string = "";
+  user: string = "XPTO-21";
+  senha: string = "Trocar@123";
+  contador:number = 0;
+  spinner: boolean = false;
+  
   login():void {
+
+    this.spinner = true;
+    
+    setTimeout(() => {
+
+      this.spinner = false;
+
+    }, 1000);
+
     if (this.contador >= 3){
-      this.color = "red"
-      this.msn = "Usuário Bloqueado!"
-    } else if (this.email != this.emailCadastrado) {
-      this.color = "red"
-      this.msn = "Acesso negado, usuário incorreto"
-    } else if (this.password != this.passwordCadastrado) {
-      this.color = "red"
-      this.msn = "Acesso negado, senha incorreta"
-      this.contador = this.contador + 1
-    } else if (this.email == this.emailCadastrado && this.password == this.passwordCadastrado) {
-      this.color = "green"
-      this.msn = "Logado!"
-      this.contador = 0
+
+      this.classe.color = "text-danger";
+      this.msn = "Usuário Bloqueado!";
+
+    } else if (this.usuario.userId != this.user) {
+
+      this.classe.color = "text-danger";
+      this.msn = "Acesso negado, usuário incorreto";
+
+    } else if (this.usuario.password != this.senha) {
+
+      this.classe.color = "text-danger";
+      this.msn = "Acesso negado, senha incorreta";
+      this.contador = this.contador + 1;
+
+    } else if (this.usuario.userId == this.user && this.usuario.password == this.senha) {
+
+      this.classe.color = "text-success";
+      this.msn = "Logado!";
+      this.contador = 0;
+
     }
   }
 }
